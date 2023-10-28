bf7e4d55a29dc926dc67f043b799e3425c29856f
Alabi Ayobami <arocket04@gmail.com>
2023-10-28 10:59:45 +0100

    first push

---
+++ main.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+const bodyParser = require('body-parser');
+const paystack_secret_key = 'sk_test_59cff139324b201a3f4bff60dc0675f9a281edb4';
+
+app.use(bodyParser.json());
+
+app.post('/create_account', (req, res) => {
+    const amount = req.body.amount;  // get the payment amount from the request body
+const userPhoneNumberAndEmailConcat= req.body.phone +"prestigiousDataPlugServices@gmail.com"
+    // Call the Paystack API to initialize a transaction
+    const options = {
+        url: 'https://api.paystack.co/transaction/initialize',
+        headers: {
+            'Authorization': `Bearer ${paystack_secret_key}`,
+            'Content-Type': 'application/json'
+        },
+        json: {
+            'email': userPhoneNumberAndEmailConcat,
+            'amount': amount
+        }
+    };
+
+    request.post(options, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            const access_code = body.data.access_code;
+            const authorization_url =body.data.authorization_url;
+            const reference_code = body.data.reference;
+            res.json({ 'message': 'Transaction initialized successfully', 'access_code': access_code, "authorization_url":authorization_url,"reference":reference_code });
+            console.log(access_code);
+        } else {
+            res.json({ 'message': 'Failed to initialize transaction', 'error': body });
+            console.log('message: Failed to initialize transaction', 'error: ', body)
+        }
+    });
+});
+
+const port = 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
